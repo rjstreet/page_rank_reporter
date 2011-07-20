8b00a9a31f10d585077a2f729c89fb1f89294f31
Josh Street <rjstreet@nephilim.local>
2011-07-19 23:19:44 -0400

    first mongo

--- reporting.rb
+++ reporting.rb
@@ -1,7 +1,37 @@
 require 'rubygems'
 require 'sinatra'
+require 'google_rank_check'
+require 'bing_rank_check'
+require 'googlecharts'
+require 'mongo'
 
 get '/' do
   "Are you supposed to be here?"
 end
 
+get '/update_ranks' do
+  coll = get_stats_collection 
+  google = GoogleRankChecker.new
+  bing = BingRankChecker.new
+end
+
+get '/keywords' do
+  keywords = get_keywords()
+  keywords.each {|word| puts word }
+end
+
+def get_keywords
+  db = Mongo::Connection.new.db("app624994")
+  auth = db.authenticate( 'reporter', 'report12max' )
+  coll = db.collection( "keywords" )
+  keywords = Array.new
+  coll.find().each {|row| array.add row[keyword]}
+  keywords
+end
+
+def get_stats_collection
+  db = Mongo::Connection.new.db("app624994")
+  auth = db.authenticate( 'reporter', 'report12max' )
+  coll = db.collection( "stats" )
+  coll
+end
