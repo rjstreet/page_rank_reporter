f8d8258e76dc774a761aa170ebacfda5f74c2c0c
Josh Street <rjstreet@nephilim.local>
2011-07-24 18:30:22 -0400

    GChart

--- reporting.rb
+++ reporting.rb
@@ -20,8 +20,7 @@ get '/charts' do
     coll = get_stats_collection
     coll.find( :keyword => keyword, :engine => "google" ).each {| row | google_stats[ row[ "date" ] ] = row[ "rank" ] }
     coll.find( :keyword => keyword, :engine => "bing" ).each {| row | bing_stats[ row[ "date" ] ] = row[ "rank" ] }
-    #return Gchart.bar(:data => [google_stats, bing_stats],  :bar_colors => 'FF0000,00FF00', :stacked=>'false', :orientation=>'horizontal')
-    return bing_stats
+    return Gchart.bar(:data => [google_stats, bing_stats],  :bar_colors => 'FF0000,00FF00', :stacked=>'false', :orientation=>'horizontal')
   end
 end
 
